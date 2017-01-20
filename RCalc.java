5e41ed5350dee5fbc953ddea73f66982880dc046
Ronald Nettey <ronaldnettey@yahoo.com>
2017-01-20 09:03:29 +0000

    Multiplication done

--- RCalc.java
+++ RCalc.java
@@ -3,12 +3,10 @@ public class RCalc{
         return a+b;
     }
      public float subtract(float a, float b){
-        
         return a-b;
     }
      public float multiply(float a, float b){
-        
-        return 0;
+        return a*b;
     }
      public float divide(float a, float b){
         
