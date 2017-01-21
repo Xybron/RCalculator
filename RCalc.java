20a1248d0dd0f9907a65e36718b506453c79c509
Dorcas <delidoe95@gmail.com>
2017-01-21 14:58:09 +0000

    committed Ronalds

--- RCalc.java
+++ RCalc.java
@@ -1,29 +1,34 @@
 public class RCalc{
-    public static void main(String[] args){
-        if(args[0].equalsIgnoreCase("add")){
-            double num1 = Double.parseDouble(args[1]);
-            double num2 = Double.parseDouble(args[2]);
-
-            System.out.println(num1 + num2);
-        }
-
-        else if(args[0].equalsIgnoreCase("subtract")){
-            double num1 = Double.parseDouble(args[1]);
-            double num2 = Double.parseDouble(args[2]);
-
-            System.out.println(num1 - num2);
-        }
-         else if(args[0].equalsIgnoreCase("multiply")){
-            double num1 = Double.parseDouble(args[1]);
-            double num2 = Double.parseDouble(args[2]);
-
-            System.out.println(num1 * num2);
-        }
-
-
-
-
-
+  public static void main(String[] args){
+    if(args[0].equalsIgnoreCase("add")){
+      double num1 = Double.parseDouble(args[1]);
+      double num2 = Double.parseDouble(args[2]);
+      
+      System.out.println(num1 + num2);
     }
     
+    else if(args[0].equalsIgnoreCase("subtract")){
+      double num1 = Double.parseDouble(args[1]);
+      double num2 = Double.parseDouble(args[2]);
+      
+      System.out.println(num1 - num2);
+    }
+    else if(args[0].equalsIgnoreCase("multiply")){
+      double num1 = Double.parseDouble(args[1]);
+      double num2 = Double.parseDouble(args[2]);
+      
+      System.out.println(num1 * num2);
+    }
+    
+    else if(args[0].equalsIgnoreCase("divide")){
+      double num1 = Double.parseDouble(args[1]);
+      double num2 = Double.parseDouble(args[2]);
+      
+      System.out.println(num1 / num2);
+    }
+    
+    
+    
+  }
+  
 }
